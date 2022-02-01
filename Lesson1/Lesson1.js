1420ee7c34951711e113dd40dbe9dbd89a1cb9bc
Stebivka Yrii <markuius54621490@gmail.com>
2022-02-01 20:47:47 +0200

    commit_hw_js1

---
+++ Lesson1/Lesson1.js
@@ -0,0 +1,77 @@
+//Створити змінні. Присвоїти кожному з них значення
+var hel = 'hello';
+var owu = 'owu';
+var com = 'com';
+var ua = 'ua';
+var one = 1;
+var ten = 10;
+var nine = -999;
+var hundred = 123;
+var PI = 3.14;
+var two = 2.7;
+var sixteen = 16;
+console.log(true);
+console.log(false);
+// Вивести кожну змінну за допомогою: console.log ,
+console.log(hel);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(nine);
+console.log(hundred);
+console.log(PI);
+console.log(two);
+console.log(sixteen);
+//Переприсвоїти кожній змінній з завдання значення на довільне.
+//   Вивести кожну змінну за допомогою: console.log , alert, document.write
+var hel = '0';
+var owu = '1';
+var com = '2';
+var ua = '3';
+var one = 4;
+var ten = 5;
+var nine = 6;
+var hundred = 7;
+var PI = 8;
+var two = 9;
+var sixteen = 10;
+console.log(hel);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(one);
+console.log(ten);
+console.log(nine);
+console.log(hundred);
+console.log(PI);
+console.log(two);
+console.log(sixteen);
+
+let firstName = ('Stebivka');
+let middleName = ('Yrii');
+let lastName = ('Viktorovuch');
+let person = (firstName+' '+middleName+' '+lastName);
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+//- Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(5 >= 6);
+console.log(10 == 10);
+console.log(10 === 10);
+console.log(10 != 10);
+console.log(10 < 10);
+console.log(10 > 10);
+console.log(123 < 123);
+console.log(123 == 123);
