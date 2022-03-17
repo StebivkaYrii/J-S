32db53418e15ab5f0afb9e7971bbe00993182599
StebivkaYrii <markuius54621490@gmail.com>
2022-03-17 20:47:14 +0200

    L13.js

---
+++ Lesson13/L13GW.js
@@ -0,0 +1,77 @@
+//створити пошагово розпорядок дня
+// setTimeout(()=>{
+//     let counter = 0;
+//
+// },1000)
+
+setTimeout(() => {
+    let counter = 0;
+    console.log('7:50 - подзвонив будильник', counter);
+    setTimeout(() => {
+        counter++;
+        console.log('8:10 - встаю і одягаюсь', counter);
+        setTimeout(() => {
+            counter++;
+            console.log('8:20 - роблю чай, ставлю сковорідку на плиту, іду в ванну', counter);
+            setTimeout(() => {
+                counter ++;
+                console.log('8:40 - взуваюсь одягаю верхній одяг виходжу з дому', counter);
+                setTimeout(() => {
+                    counter ++;
+                    console.log('8:58 - приходжу на роботу відкриваю магазин', counter);
+                    setTimeout(() => {
+                        counter ++;
+                        console.log('10:00 - роблю каву дивлюсь відоси Сергія', counter);
+                        setTimeout(() => {
+                            counter ++;
+                            console.log('11:00 - розпалюю в котлі ', counter);
+                            setTimeout(() => {
+                                counter ++;
+                                console.log('14:00 - закриваю магазин', counter);
+                                setTimeout(() => {
+                                    counter ++;
+                                    console.log('14:30 - заходжу до бабці в гості', counter);
+                                    setTimeout(() => {
+                                        counter ++;
+                                        console.log('15:30 - іду додому', counter);
+                                        setTimeout(() => {
+                                            counter ++;
+                                            console.log('16:00 - прийшов, зготував поїсти пошарився подивився новини', counter);
+                                            setTimeout(() => {
+                                                counter ++;
+                                                console.log('18:00 - сідаю за компютер починаю дивитись що по завданнях із Js', counter);
+                                                setTimeout(() => {
+                                                    counter ++;
+                                                    console.log('20:00 - вечеря', counter);
+                                                    setTimeout(() => {
+                                                        counter ++;
+                                                        console.log('21:00 - продовжую сидіти за компютером', counter);
+                                                        setTimeout(() => {
+                                                            counter ++;
+                                                            console.log('23:00 - іду віддихати, дивлюсь меми на толефоні', counter);
+                                                            setTimeout(() => {
+                                                                counter ++;
+                                                                console.log('00:00 - спати', counter);
+                                                            },1000)
+                                                        },1000)
+                                                    },1000)
+                                                },1000)
+                                            },1000)
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        }, 1000)
+    }, 1000);
+}, 1000)
+
+// setTimeout(() => {
+//     counter ++;
+//     console.log('7:50 - ', counter);
+//
+// },1000)
+
